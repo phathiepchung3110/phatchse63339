0cf129d1bb075ebaa96c819011872752246ff79e
84934007381 <phathiepchung@outlook.com>
2020-11-19 17:17:40 +0700

    good job

--- Api/Controllers/JobsController.cs
+++ Api/Controllers/JobsController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Service.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+//GOODJOB
 namespace Api.Controllers
 {
 	[Route("api/jobs")]
